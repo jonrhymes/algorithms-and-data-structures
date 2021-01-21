ed3ed7c823ab3e97a3f4934779c7de4f91fa269e
jonrhymes <jonrhymes@gmail.com>
2021-01-21 15:32:19 -0500

    Added Graph intro

---
+++ Data-Structures/Graphs/1_Graph_Intro.js
@@ -0,0 +1,15 @@
+// GRAPHS - Nodes and Connections
+// Used for:
+// - Social Networks, Location Mapping, Routing Algorithsm
+
+// Vertex = A Node
+// Edge = Connection between Nodes
+// Weighted/Unweighted = Values assigned to distances between Vertices
+// Directed/Undirected = Directions assigned to distances between Vertices
+
+
+// Adjancency Matrix - Takes up more space, Slower to iterate over all edges, BUT...
+// Faster to look up specific edge
+
+// Adjacency List - Takes up LESS space, FASTER to iterate over all edges, BUT...
+// SLOWER to look up specific edge
